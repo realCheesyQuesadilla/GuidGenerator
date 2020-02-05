5534c8004f2e91c206efe1814211430fc1173833
David Emily <daeth5@mail.missouri.edu>
2020-02-05 16:45:03 -0600

    gives consumer guid through alert box

---
+++ popup.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', onclick, false)
+
+    function onclick () {
+        alert(generateUuid());
+    }
+
+    // Public Domain/MIT
+    //Increases randomness of UUID by using timestamp
+    function generateUuid () { 
+        var d = new Date().getTime();
+        var d2 = (performance && performance.now && (performance.now()*1000)) || 0;
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16;
+            if(d > 0){
+                r = (d + r)%16 | 0;
+                d = Math.floor(d/16);
+            } else {
+                r = (d2 + r)%16 | 0;
+                d2 = Math.floor(d2/16);
+            }
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
+}, false)
