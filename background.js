423d431c54326d79c8331e49fb4c9627dd6c00cc
David Emily <daeth5@mail.missouri.edu>
2020-02-07 16:18:23 -0600

    updated version number and removed some logs

--- background.js
+++ background.js
@@ -16,10 +16,8 @@ chrome.commands.onCommand.addListener(function (command) {
       
         try {
           var successful = document.execCommand('copy');
-          var msg = successful ? 'successful' : 'unsuccessful';
-          console.log('Fallback: Copying text command was ' + msg);
         } catch (err) {
-          console.error('Fallback: Oops, unable to copy', err);
+          console.log('Unable to copy guid');
         }
       
         document.body.removeChild(textArea);
