2bcff2c5121443f83346aab6e57298d2f5fe5973
Debian Dave <debian.dave@pm.me>
2022-01-01 09:12:42 -0600

    Update generateUuid.js

--- generateUuid.js
+++ generateUuid.js
@@ -1,22 +1,3 @@
-//................................................................................................................................
-//..................................dddd............... bbb.............................dddd.....................iiii.......dddd..
-//..................................dddd............... bbb.............................dddd.....................iiii.......dddd..
-//..................................dddd............... bbb.............................dddd................................dddd..
-//.mmmmmmmmmmmmmm....aaaaaa....ddddddddd..eeeeee....... bbbbbbbb.bbyy...yyyy.......ddddddddd..aaaaaa..avvv..vvvv.iiii..ddddddddd..
-//.mmmmmmmmmmmmmmm..aaaaaaaa..dddddddddd.eeeeeeee...... bbbbbbbbb.byyy..yyyy......dddddddddd.aaaaaaaa.avvv..vvvv.iiii.dddddddddd..
-//.mmmmm.mmmmmmmmm.maaa.aaaaaadddd.ddddddeee.eeee...... bbbb.bbbbbbyyy..yyyy..... dddd.ddddddaaa.aaaaaavvv.vvvv..iiiiidddd.ddddd..
-//.mmmm..mmmm..mmmm....aaaaaaaddd...dddddeee..eeee..... bbb...bbbbbyyy.yyyy...... ddd...dddd....aaaaaa.vvvvvvvv..iiiiiddd...dddd..
-//.mmmm..mmmm..mmmm.aaaaaaaaaaddd...dddddeeeeeeeee..... bbb...bbbb.yyyyyyyy...... ddd...dddd.aaaaaaaaa.vvvvvvvv..iiiiiddd...dddd..
-//.mmmm..mmmm..mmmmmaaaaaaaaaaddd...dddddeeeeeeeee..... bbb...bbbb.yyyyyyy....... ddd...dddddaaaaaaaaa.vvvvvvv...iiiiiddd...dddd..
-//.mmmm..mmmm..mmmmmaaa.aaaaaaddd...dddddeee........... bbb...bbbb..yyyyyy....... ddd...dddddaaa.aaaaa..vvvvvv...iiiiiddd...dddd..
-//.mmmm..mmmm..mmmmmaaa.aaaaaadddd.ddddddeee..eeee..... bbbb.bbbbb..yyyyyy....... dddd.ddddddaaa.aaaaa..vvvvvv...iiiiidddd.ddddd..
-//.mmmm..mmmm..mmmmmaaaaaaaaa.dddddddddd.eeeeeeee...... bbbbbbbbb...yyyyy.........dddddddddddaaaaaaaaa..vvvvv....iiii.dddddddddd..
-//.mmmm..mmmm..mmmm.aaaaaaaaa..ddddddddd..eeeeee....... bbbbbbbb.....yyyy..........ddddddddd.aaaaaaaaa...vvvv....iiii..ddddddddd..
-//...................................................................yyyy.........................................................
-//..................................................................yyyy..........................................................
-//................................................................byyyyy..........................................................
-//................................................................byyyy...........................................................
-//................................................................................................................................
 
 window.mynamespace = window.mynamespace || {};
 mynamespace.generateUuid = function generateUuid () { 
